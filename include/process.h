848defc3bce91e766005668ae02e9b98acfa8600
Huang Yuming <987297028@qq.com>
2024-10-27 22:03:07 +0800

    modify include/process.h
    修改进程结构体定义

--- include/process.h
+++ include/process.h
@@ -4,21 +4,13 @@
 #define MAX_PROCESS_NUMBER 10
 #define MAX_PROCESS_NAME 30
 
-typedef int ProcessDataType;
-
 typedef struct Process {
-	ProcessDataType id; // 进程序号
-	ProcessDataType pid; // 进程号
+	int pid; // 进程号
 	char name[MAX_PROCESS_NAME]; // 进程名
-	ProcessDataType submit_time, // 提交时间
-									need_run_time, // 尚需运行时间
-									start_time, // 开始运行时间
-									end_time, // 结束运行时间
-									actual_run_time, // 实际运行时间
-									turnaround_time, // 周转时间
-									weighted_turnaround_time; // 带权周转时间
-	ProcessDataType priority; // 优先级（越大，优先级越高）
-	ProcessDataType run_order; // 运行次序
+	double arrive_time; // 到达时间
+	double run_time; // 运行时间
+	int priority; // 优先级（越大，优先级越高）
+	int run_order; // 运行次序
 } Process;
 
 Process process[MAX_PROCESS_NUMBER];
